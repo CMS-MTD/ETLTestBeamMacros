a366da77a5c29b086fbdff921a4ac79dd8c39b8a
Claudio San Martin <claudio.sanmartinval@gmail.com>
2022-03-13 05:01:55 -0500

    Adding two IHEP sensors (W1_III & W7_I)

---
+++ macros/plot_IHEP_W7_I.C
@@ -0,0 +1,67 @@
+#include "map_plotter.C"
+
+# ifndef __CINT__
+int main(int argc, char **argv)
+{
+	map_plotter mp;
+	mp.tag = "IHEP_W7_I_2x2"; //This defines names of all output files
+	mp.sensor_name="IHEP W7"; //Must be contained in sensor name as defined in AirTable. This determines which scope channels to consider.
+	mp.chainPath = "root://cmseos.fnal.gov//store/group/cmstestbeam/SensorBeam2022/LecroyScope/RecoData/TimingDAQRECO/RecoWithTracks/v1/";
+	mp.debug=false;
+
+	//Define run range
+	//Note: will try to load every run in this range, even if it doesn't exist (so, expect some harmless complaints.)
+	mp.run_end = new vector<int>{53814};
+	mp.run_start = new vector<int>{53549};
+
+
+	//Define rotation angle and manual adjustments of x and y
+	mp.angle = new vector<float>{0}; //degrees
+	mp.x_offset= new vector<float>{0.0}; //mm
+	mp.y_offset= new vector<float>{0.0}; //mm
+	
+	mp.dut_index = 7; //center of telescope
+	mp.maxTrackChi2 = 50;
+
+	//Define xy binning and ranges [mm]
+	mp.nbinsX=100;//30;
+	mp.nbinsY=100;//10;
+	mp.rebinFactor=1; //coarser binning for maps of gain, timing (finer for efficiency)
+	mp.minX=-5.0; mp.maxX=0.5;
+	mp.minY=-4.0; mp.maxY=-0.5;
+
+	//Define amplitude, time binning and range, and scope saturation.
+	mp.saturation =380.;
+	mp.nbinsAmp=50;
+	mp.minAmp=0; mp.maxAmp=mp.saturation;
+	mp.nbinsTime=60;
+	mp.minTime=0; mp.maxTime=1500; //picoseconds
+	mp.delta_t_sign=-1; //
+	
+	//define threshold for LGAD hits, and range for photek
+	mp.hitThres=vector<float>(mp.npad+1,50.);
+	mp.photekMin=200;
+	mp.photekMax=350;
+
+	// Define geometric boundaries for 1D "slices"
+	mp.xSliceMin={-3.5,-2.2};
+	mp.xSliceMax={-3.3,-1.8};
+	mp.ySliceMin={-3.2,-1.7};
+	mp.ySliceMax={-2.7,-1.4};
+
+	//Force range for colz maps. -1 is ignored.
+	// mp.zMinEff=0.6; mp.zMaxEff=-1.; 
+	mp.zMinEff=0.; mp.zMaxEff=-1.; 
+	mp.zMinGain=120.; mp.zMaxGain=-1.;  // Gain means MPV amplitude in mV, here...
+	// mp.zMinSigmat=0.01e-9; mp.zMaxSigmat=0.08e-9; 
+	mp.zMinSigmat=10; mp.zMaxSigmat=50; 
+	mp.zMinDeltat=-1; mp.zMaxDeltat=-1;
+	// mp.zMinDeltat=-300; mp.zMaxDeltat=-1.;
+
+
+	//Do everything
+    mp.makeMaps();
+    
+
+}
+# endif
